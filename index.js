cc4e9a35d1e043d9d8a2af1703cc20e374e735e9
Juan Cabria <Juan.Cabria@gmail.com>
2019-11-26 21:33:44 +0200

    fix

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ app.delete('/notes/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
